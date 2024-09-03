6986fe749d41cda96d3191162ca26d9727180732
minitsingh <MINIT SINGH@DESKTOP-MGUTKMH.mshome.net>
2024-09-03 10:29:42 +0530

    for jenkins practice during study

---
+++ SeleniumProject/src/test/java/pac1/ConatctTest.java
@@ -0,0 +1,29 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ConatctTest {
+	
+	@Test
+	public void createConatctTest()
+	{
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		System.out.println("execute conatct test");
+	}
+	@Test
+	public void modifyConatctTest()
+	{
+		System.out.println("execute modifyConatctTest");
+	}
+
+
+}
